e5b89078d5c0517ce7e356207056e91a21522800
Nicky Kriplani <aniketnickykriplani@gmail.com>
2023-02-08 02:18:45 -0500

    added node code and reminders for later

--- binomial_heap.cpp
+++ binomial_heap.cpp
@@ -1,14 +1,31 @@
 #include "binomial_heap.h"
 
-
-
 /***************************************************************************************************
 *                                                                                                  *
 *                                                                                                  *
-*                            binomial_heap::binomial_node implementation                           *
+*                                binomial_heap::node implementation                                *
 *                                                                                                  *
 *                                                                                                  *
 ***************************************************************************************************/
+
+template<typename T, typename Comp>
+binomial_heap<T, Comp>::node::node() {}
+
+template<typename T, typename Comp>
+binomial_heap<T, Comp>::node::node(
+    const binomial_heap<T, Comp>::node& rhs
+) { this->operator=(rhs); }
+
+template<typename T, typename Comp>
+binomial_heap<T, Comp>::node& binomial_heap<T, Comp>::node::operator=(const node& rhs) {
+    return *this;
+}
+
+template<typename T, typename Comp>
+binomial_heap<T, Comp>::node::~node() { delete_children(); children.clear(); }
+
+template<typename T, typename Comp>
+void binomial_heap<T, Comp>::node::delete_children() { for(node* child: children) delete child; }
 
 
 /***************************************************************************************************
@@ -19,12 +36,18 @@
 *                                                                                                  *
 ***************************************************************************************************/
 template<typename T, typename Comp>
-binomial_heap::binomial_heap() {}
+binomial_heap<T, Comp>::binomial_heap() {}
 
 template<typename T,  typename Comp>
-binomial_heap::binomial_heap(const binomial_heap& rhs) {}
+binomial_heap<T, Comp>::binomial_heap(const binomial_heap& rhs) { this->operator=(rhs); }
 
 template<typename T, typename Comp>
-binomial_heap& binomial_heap::operator=(const binomial_heap& rhs) {
+binomial_heap<T, Comp>& binomial_heap<T, Comp>::operator=(const binomial_heap& rhs) {
     return *this;
 }
+
+template<typename T, typename Comp>
+binomial_heap<T, Comp>::~binomial_heap() { delete_trees(); trees.clear(); }
+
+template<typename T, typename Comp>
+void binomial_heap<T, Comp>::delete_trees() { for(node* tree: trees) delete tree; }
--- binomial_heap.h
+++ binomial_heap.h
@@ -8,10 +8,27 @@ public:
     binomial_heap(const binomial_heap& rhs);
     binomial_heap(binomial_heap&&) = default;
     binomial_heap& operator=(const binomial_heap& rhs);
+    ~binomial_heap();
+    void delete_trees();
+    /*necessary functionality
+        report min
+        union
+        insert
+        extract min
+        decrease key --> consider that maybe the binomial_node struct should be public so that
+                         decrease keys can be called with pointers 
+        delete
+    */
 private:
-    struct binomial_node {
+    struct node {
+        node();
+        node(const node& rhs);
+        node(node&&) = default;
+        node& operator=(const node& rhs);
+        ~node();
+        void delete_children();
             T key;
-            std::list<binomial_node> children;
+            std::list<node*> children;
     };
-    std::list<binomial_node> trees;
+    std::list<node*> trees;
 };
