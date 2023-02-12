6c6e3506ebbc96732ff6472278309a5cea902b55
Nicky Kriplani <aniketnickykriplani@gmail.com>
2023-02-12 17:35:22 -0500

    lots of documentation and cleaning up

--- binomial_heap.h
+++ binomial_heap.h
@@ -30,6 +30,11 @@
 *          make a decision based on how tangible they are.                                         *
 ***************************************************************************************************/
 
+/**
+ *  @brief  A binomial heap that supports fast insertion and merging
+ *  @tparam T the type of the key that wil be stored in the heap
+ *  @tparam Comp the comparison function that will be used for heap-ordering. defaults to std::less
+ */
 template<typename T, typename Comp = std::less<T>>
 class binomial_heap {
     struct node;
@@ -43,10 +48,10 @@ public:
     binomial_heap& operator=(const binomial_heap& rhs);
     binomial_heap& operator=(binomial_heap&& rhs);
     ~binomial_heap();
-    size_t size();
-    bool empty();
-    iterator find(T key);
-    T front();
+    size_t size() const;
+    bool empty() const;
+    iterator find(T key) const;
+    T min() const;
     T extract();
     void merge(binomial_heap& rhs);
     iterator insert(T key);
@@ -85,7 +90,7 @@ private:
     };
     Comp compare;
     std::forward_list<node*> trees;
-    node* min;
+    node* _min;
     size_t _size;
 };
 
@@ -199,7 +204,7 @@ void binomial_heap<T, Comp>::node::delete_children() {
 /**
  *  @brief      Searches for a node in this tree with a particular node. Time complexity is linear
  *              with the number of nodes in the tree.
- *  @param[in]  target  the key to be found
+ *  @param[in]  target the key to be found
  *  @param[in]  compare the comparison function to test if the key has been found
  *  @return     if a node with the valid key is found, the pointer to the first found occurrence of
  *              that value
@@ -220,7 +225,7 @@ typename binomial_heap<T, Comp>::node* binomial_heap<T, Comp>::node::search(
 
 /**
  *  @brief      Merges two trees in constant time, making the smaller of the two roots the new root
- *  @param[in]  to_merge    the other tree that this tree is to be merged with
+ *  @param[in]  to_merge the other tree that this tree is to be merged with
  *  @return     The new root to the tree to be replaced in the list
  */
 template<typename T, typename Comp>
@@ -252,13 +257,13 @@ typename binomial_heap<T, Comp>::node* binomial_heap<T, Comp>::node::promote(
 template<typename T, typename Comp>
 binomial_heap<T, Comp>::binomial_heap(const Comp& compare) : 
     compare(compare),
-    min(nullptr),
+    _min(nullptr),
     _size(0) {}
 
 /**
  *  @brief      Range constructor for the binomial_heap class
- *  @param[in]  start   the beginning of the range to be inserted into the heap
- *  @param[in]  stop    the end of the range to be inserted into the heap
+ *  @param[in]  start the beginning of the range to be inserted into the heap
+ *  @param[in]  stop the end of the range to be inserted into the heap
  *  @param[in]  compare the comparison functor for heap-ordering, defaults to std::less<T>
  */
 template<typename T, typename Comp>
@@ -267,31 +272,30 @@ binomial_heap<T, Comp>::binomial_heap(
     InputIterator start,
     InputIterator stop,
     const Comp& compare
-) : compare(compare), min(nullptr), _size(0) { multi_insert(start, stop); }
+) : compare(compare), _min(nullptr), _size(0) { multi_insert(start, stop); }
 
 /**
- *  @brief 
- *  @param[in]  rhs 
+ *  @brief      Copy constructor for the binomial_heap class. Performs a deep copy.
+ *  @param[in]  rhs the binomial_heap whose contents are to be copied
  */
 template<typename T,  typename Comp>
 binomial_heap<T, Comp>::binomial_heap(const binomial_heap<T, Comp>& rhs) { this->operator=(rhs); }
 
 /**
- *  @brief 
- *  @param[in]  rhs 
+ *  @brief      Move constructor for the binomial_heap class
+ *  @param[in]  rhs the binomial_heap whose contents are to be moved
  */
 template<typename T, typename Comp>
 binomial_heap<T, Comp>::binomial_heap(binomial_heap<T, Comp>&& rhs) { this->operator=(rhs); }
 
 /**
- *  @brief 
- *  @param[in]  rhs 
- *  @return 
+ *  @brief      Assignment operator for the binomial_heap class. Performs a deep copy.
+ *  @param[in]  rhs the binomial_heap to be copied
+ *  @return     this binomial_heap by reference for operator chaining
  */
 template<typename T, typename Comp>
 binomial_heap<T, Comp>& binomial_heap<T, Comp>::operator=(const binomial_heap<T, Comp>& rhs) {
     delete_trees();
-    trees.clear();
     compare = rhs.compare;
     _size = rhs._size;
     for(node* tree: rhs.trees) { trees.push_front(new node(*tree)); }
@@ -301,41 +305,50 @@ binomial_heap<T, Comp>& binomial_heap<T, Comp>::operator=(const binomial_heap<T,
 }
 
 /**
- *  @brief 
- *  @param[in]  rhs 
- *  @return 
+ *  @brief      Move assignment operator for the binomial_heap class.
+ *  @param[in]  rhs the binomial_heap to be moved
+ *  @return     this binomial_heap by reference for operator chaining
  */
 template<typename T, typename Comp>
 binomial_heap<T, Comp>& binomial_heap<T, Comp>::operator=(binomial_heap<T, Comp>&& rhs) {
     delete_trees();
-    trees.clear();
     compare = std::move(rhs.compare);
     trees = std::move(rhs.trees);
-    min = std::move(rhs.min);
+    _min = std::move(rhs._min);
     _size = std::move(rhs._size);
     return *this;
 }
 
 /**
- *  @brief 
+ *  @brief  Destructor for the binomial_heap class
  */
 template<typename T, typename Comp>
 binomial_heap<T, Comp>::~binomial_heap() { delete_trees(); }
 
+/**
+ *  @brief  Gets the size of the heap
+ *  @return the size of the heap
+ */
 template<typename T, typename Comp>
-size_t binomial_heap<T, Comp>::size() { return _size; }
+size_t binomial_heap<T, Comp>::size() const { return _size; }
 
+/**
+ *  @brief  Returns whether or not the heap is empty
+ *  @return true if the heap has zero elements. Otherwise,
+ *          false
+ */
 template<typename T, typename Comp>
-bool binomial_heap<T, Comp>::empty() { return !_size; }
+bool binomial_heap<T, Comp>::empty() const { return !_size; }
 
 
 /**
- *  @brief 
- *  @param[in]  key 
- *  @return 
+ *  @brief      Finds the first occurrence of a certain key in the heap by iterating through
+ *              all of the values until it finds an element with that key. Requires linear time.
+ *  @param[in]  key the key to be searched for in the heap
+ *  @return     an iterator containing the element that was searched for in the heap
  */
 template<typename T, typename Comp>
-typename binomial_heap<T, Comp>::iterator binomial_heap<T, Comp>::find(T key) {
+typename binomial_heap<T, Comp>::iterator binomial_heap<T, Comp>::find(T key) const {
     for(node* tree: trees) {
         node* found = tree->search(key);
         if(found) return iterator(found);
@@ -344,63 +357,67 @@ typename binomial_heap<T, Comp>::iterator binomial_heap<T, Comp>::find(T key) {
 }
 
 /**
- *  @brief 
- *  @return 
+ *  @brief  Gets the value of the minimum element in the heap
+ *  @return the value of the minimum element in the heap.
  */
 template<typename T, typename Comp>
-T binomial_heap<T, Comp>::front() { if(min) return min->key; throw new std::out_of_range("Empty"); }
+T binomial_heap<T, Comp>::min() const {
+    if(_min) return _min->key;
+    throw new std::out_of_range("Empty");
+}
 
 /**
- *  @brief 
- *  @return 
+ *  @brief  Extracts the minimum element from the heap. O(log n) time.
+ *  @return the value of the minimum element in the heap.
  */
 template<typename T, typename Comp>
 T binomial_heap<T, Comp>::extract() {
-    T min_val = min->key;
-    trees.remove(min);
-    min->children.reverse();
-    merge_lists(std::move(min->children));
-    min->children = std::forward_list<node*>();
-    delete min;
+    T min_val = _min->key;
+    trees.remove(_min);
+    _min->children.reverse();
+    merge_lists(std::move(_min->children));
+    _min->children = std::forward_list<node*>();
+    delete _min;
     set_min();
     --_size;
     return min_val;
 }
 
 /**
- *  @brief 
- *  @param rhs 
+ *  @brief          Merges two heaps, emptying the passed heap. O(log n) time.
+ *  @param[in, out] rhs the heap to be emptied and merged with this heap
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::merge(binomial_heap<T, Comp>& rhs) {
     _size += rhs._size;
-    if(compare(rhs.min->key, min->key)) min = rhs.min;
+    if(compare(rhs._min->key, _min->key)) _min = rhs._min;
     //unfinished
 }
 
 /**
- *  @brief 
- *  @param[in]  key 
- *  @return 
+ *  @brief      Inserts a key into the heap. O(1) am. time.
+ *  @param[in]  key the key to be inserted into the heap
+ *  @return     an iterator containing the node that was just inserted into the heap
  */
 template<typename T, typename Comp>
 typename binomial_heap<T, Comp>::iterator binomial_heap<T, Comp>::insert(T key) {
     ++_size;
     node* new_tree = new node(key);
     trees.push_front(new_tree);
-    if(!min) {
-        min = new_tree;
+    if(!_min) {
+        _min = new_tree;
     }
-    else if(compare(new_tree->key, min->key)) { min = new_tree; }
+    else if(compare(new_tree->key, _min->key)) { _min = new_tree; }
     fast_zip();
     return iterator(new_tree);
 }
 
 /**
- *  @brief 
- *  @param[in]  start 
- *  @param[in]  stop 
- *  @return 
+ *  @brief      Inserts a range of elements into the heap
+ *  @param[in]  start the beginning of the range to be inserted into the heap
+ *  @param[in]  stop the end of the range to be inserted into the heap
+ *  @return     a vector containing the respective iterators for each of the elements that were
+ *              inserted into the heap.
  */
 template<typename T, typename Comp>
 template<class InputIterator>
@@ -415,30 +432,34 @@ std::vector<typename binomial_heap<T, Comp>::iterator> binomial_heap<T, Comp>::m
 }
 
 
-//if the key becomes equivalent to another key, it->data bubbles above, necessary for remove
 /**
- *  @brief 
- *  @param[in, out] it 
- *  @param[in]      new_key 
+ *  @brief          Decreases the key of the node contained within the passed iterator. If the
+ *                  key passed in is equivalent to an existing key within the tree of the 
+ *                  referenced node, the referenced node will bubble above the existing equivalent
+ *                  node (for remove()). O(log n) time.
+ *  @param[in, out] it an iterator containing the node whose key is to be decreased
+ *  @param[in]      new_key the value the key is to be decreased to
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::decrease_key(
     const binomial_heap<T, Comp>::iterator& it,
     T new_key
-) {}
+) {
+    if(!compare(new_key, it.data->key)) throw new std::invalid_argument("Invalid new key.");
+}
 
 /**
- *  @brief 
- *  @param[in]  it 
+ *  @brief      Removes the specified element from the heap
+ *  @param[in]  it iterator of the element that is to be removed
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::remove(typename binomial_heap<T, Comp>::iterator&& it) {
-    decrease_key(it, min->key);
+    decrease_key(it, _min->key);
     extract();
 }
 
 /**
- *  @brief 
+ *  @brief  Empties the heap, destroying all elements. Requires linear time.
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::delete_trees() {
@@ -448,17 +469,17 @@ void binomial_heap<T, Comp>::delete_trees() {
 }
 
 /**
- *  @brief 
+ *  @brief  Finds the min value of all of the roots. Requires logarithmic time.
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::set_min() {
-    if(trees.empty()) { min = nullptr; return; }
-    min = trees.front();
-    for(node* tree: trees) if(compare(tree->key, min->key)) min = tree;
+    if(trees.empty()) { _min = nullptr; return; }
+    _min = trees.min();
+    for(node* tree: trees) if(compare(tree->key, _min->key)) _min = tree;
 }
 
 /**
- *  @brief 
+ *  @brief  Zips up the tree list, merging together duplicate degree trees.
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::zip() {
@@ -473,9 +494,9 @@ void binomial_heap<T, Comp>::zip() {
     }
 }
 
-//zips until no duplicate found, meant for insertion
 /**
- *  @brief 
+ *  @brief  Zips up the tree list until it reaches any two distinct degrees, at which point merging
+ *          stops. Meant for insertion.
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::fast_zip() {
@@ -490,12 +511,11 @@ void binomial_heap<T, Comp>::fast_zip() {
 }
 
 /**
- *  @brief 
- *  @param[in]  rhs 
+ *  @brief  Merges this tree list with rhs, zipping up the list afterwards.
+ *  @param[in]  rhs the list with which this list is to be merged.
  */
 template<typename T, typename Comp>
 void binomial_heap<T, Comp>::merge_lists(
     std::forward_list<typename binomial_heap<T, Comp>::node*>&& rhs
 ) { trees.merge(rhs, [] (node* a, node* b) { return a->degree < b->degree; }); zip(); }
-
 #endif
--- heap_sort.h
+++ heap_sort.h
@@ -10,6 +10,14 @@
 #define HEAP_SORT 1
 #include "binomial_heap.h"
 
+/**
+ *  @brief Sorts the data beginning at start and ending at stop using the comparison function
+ *         provided.
+ * 
+ *  @param[in, out] start   the beginning of the range in which data is to be sorted
+ *  @param[in, out] stop    the end of the range in which data is to be sorted
+ *  @param[in]      compare the comparison function to be used for sorting, defaults to std::less
+ */
 template<class InputIterator, typename Comp = std::less<typename InputIterator::value_type>>
 void heap_sort(InputIterator start, InputIterator stop, const Comp& compare = Comp()) {
     binomial_heap<typename InputIterator::value_type, Comp> heap(start, stop, compare);
